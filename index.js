8d87a0bff2cdf6551ea87606c672932d0d6d15e5
AleKs <alexanderfranciscog@gmail.com>
2021-08-13 14:22:28 -0400

    Cambios en CSS (navbar)

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// header__nav-menu
-// header__toggle
-
 const navToggle = document.querySelector(".header__toggle");
 const navMenu = document.querySelector(".header__nav-menu");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");
+    navMenu.classList.toggle(".nav-menu_visible");
 })
