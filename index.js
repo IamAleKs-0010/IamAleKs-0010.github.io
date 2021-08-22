60ae3301f7f1630633628d66d8bd4f03e8946e12
AleKs <alexanderfranciscog@gmail.com>
2021-08-22 13:01:48 -0400

    Cambios en estructura CSS

--- index.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".header__toggle");
-const navMenu = document.querySelector(".header__nav-menu");
-
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle(".nav-menu_visible");
-})
